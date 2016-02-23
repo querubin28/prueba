4893056bf57da0f3527013ad645f0e84b42d9f39
eduardo vidal <eduardovidal@macbook-air-de-eduardo-2.local>
2016-02-22 21:41:57 -0600

    Initial Commit

---
+++ pruebagithubTests/pruebagithubTests.swift
@@ -0,0 +1,36 @@
+//
+//  pruebagithubTests.swift
+//  pruebagithubTests
+//
+//  Created by eduardo vidal on 22/02/16.
+//  Copyright © 2016 00. All rights reserved.
+//
+
+import XCTest
+@testable import pruebagithub
+
+class pruebagithubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
